9f3b167e9afecf83d3560500d6efd94ec3df5f18
Muhammad Rabiul <rixetbd@gmail.com>
2022-08-04 00:47:58 +0600

    1s

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let result = document.getElementById('result');
+let deleteOne = document.getElementById('deleteOne');
 let clear = document.getElementById('clear');
 let equal = document.getElementById('equal');
 let calculation = document.getElementById('calculation');
@@ -8,16 +9,44 @@ let multiple = document.getElementById('multiple');
 let minus = document.getElementById('minus');
 let plus = document.getElementById('plus');
 
-
 function input(value){
-    calculation.textContent = value;
+
+    let getinput = value;
+    let tesxts = calculation.innerText;
+
+    let signDected = tesxts.charAt(tesxts.length - 1);
+
+    if(calculation.innerText == 0){
+        calculation.textContent = value;
+    }else if(signDected == "%" || signDected == "/" || signDected == "*" || signDected == "-" || signDected == "+"){
+        if (value == "%" || value == "/" || value == "*" || value == "-" || value == "+"){
+            calculation.innerText = tesxts.replace(signDected, value);
+        }else{
+            calculation.textContent = calculation.innerText + getinput;
+        }
+    }else if(calculation.innerText == "%" || calculation.innerText == "/" || calculation.innerText == "*" || calculation.innerText == "-" || calculation.innerText == "+"){
+        calculation.textContent = value;
+    }else{
+        calculation.textContent = calculation.innerText + getinput;
+    }
 }
 
 clear.addEventListener('click', function(){
-    calculation.textContent = "";
-    result.textContent = "0";
+    calculation.textContent = "0";
+    result.textContent = "";
 })
 
 equal.addEventListener('click', function(){
-    result.textContent = calculation.innerHTML;
+    result.textContent = eval(calculation.innerHTML);
+})
+
+deleteOne.addEventListener('click', function(){
+
+    if(calculation.innerText.length == 1){
+        calculation.textContent = "0";
+    }else{
+        let disText = calculation.textContent;
+        let calArr = disText.slice(0, -1);
+        calculation.textContent = calArr;
+    }
 })
